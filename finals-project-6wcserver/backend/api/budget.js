0192bc0af45c70027dc3f5f33cf1b09dd62fe056
yambaolr <yambaolorinrobelle@gmail.com>
2025-09-17 21:38:16 +0800

    :bug: Fixed budget and expense API logic error

--- finals-project-6wcserver/backend/api/budget.js
+++ finals-project-6wcserver/backend/api/budget.js
@@ -12,8 +12,8 @@ router.post('/', async (req,res) => {
 });
 
 //Read budget
-router.get('/', async (req,res) => {
-    const budgets = await Budget.find();
+router.get('/:userId', async (req,res) => {
+    const budgets = await Budget.find({ user_id: req.params.userId });
     res.json(budgets);
 });
 
--- finals-project-6wcserver/backend/api/expense.js
+++ finals-project-6wcserver/backend/api/expense.js
@@ -6,15 +6,15 @@ const router = express.Router();
 
 // relative routes only;
 // Create expense
-router.post("/", async (req,res) =>{
+router.post("/", async (req,res) => {
     const expense = new Expense(req.body);
     await expense.save;
     res.json(expense);
 });
 
 // Read expense
-router.get("/", async (req,res) =>{
-    const expenses = await Expense.find();
+router.get("/:userId", async (req,res) => {
+    const expenses = await Expense.find({ user_id: req.params.userId });
     res.json(expenses);
 });
 
