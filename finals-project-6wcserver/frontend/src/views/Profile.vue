dd3a3e52a3562c1265dd595906fb762fc206d1de
yambaolr <yambaolorinrobelle@gmail.com>
2025-09-27 00:11:44 +0800

    🎨 Improved profile issue

--- finals-project-6wcserver/frontend/src/views/Profile.vue
+++ finals-project-6wcserver/frontend/src/views/Profile.vue
@@ -69,7 +69,6 @@
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
 import api from "@/api/axios";
-import { useRouter } from "vue-router"; // what??? diman nagamit??????????? or wrong var?? 
 
 export default {
   name: 'Profile',
@@ -90,7 +89,7 @@ export default {
         console.log("UserId from localStorage:", userId);
 
         if (!userId) {
-          this.$router.push("/login"); // wrong var amp
+          this.$router.push("/login");
           return;
         }
 
@@ -102,13 +101,13 @@ export default {
       } catch (err) {
         console.error("Fetch user error:", err.response ? err.response.data : err.message);
         alert("Fetch user failed: " + (err.response?.data?.message || err.message));
-        this.$router.push("/login"); // gumana??
+        this.$router.push("/login");
     }
     },
     logout() {
       // after logging out, tokens, username, userid stored will be cleared
       localStorage.clear();
-      this.$router.push("/").then(() => { // kahit maling var??? 
+      this.$router.push("/").then(() => {
         location.reload;
       });
     },
