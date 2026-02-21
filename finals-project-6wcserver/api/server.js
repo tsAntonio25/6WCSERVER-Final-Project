da9b0cfbf8c6515bae4ca4cc1638625918f7c3c6
yambaolr <yambaolorinrobelle@gmail.com>
2026-02-21 21:34:55 +0800

    ✨ Vercel deployment config

--- finals-project-6wcserver/api/server.js
+++ finals-project-6wcserver/api/server.js
@@ -21,7 +21,14 @@ const router = express.Router()
 const PORT = process.env.PORT || 3000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: process.env.NODE_ENV === "production"
+            ? "https://iponly.vercel.app"
+            : "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json()); 
 
 // database connection
